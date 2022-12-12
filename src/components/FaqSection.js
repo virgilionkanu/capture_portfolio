96dcfa963f4c7fc7f6b73f9a5628f239341144bb
virgilionkanu <virgiliocano53@gmail.com>
2022-12-12 19:50:25 -0300

    feat: Add third section of About Us page

---
+++ src/components/FaqSection.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+          consectetur.
+        </p>
+      </div>
+
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+          consectetur.
+        </p>
+      </div>
+
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+          consectetur.
+        </p>
+      </div>
+
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+          consectetur.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
