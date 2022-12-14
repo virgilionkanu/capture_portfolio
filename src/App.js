df4edcdc5558375400e68385ec5f0391a2407676
virgilionkanu <virgiliocano53@gmail.com>
2022-12-14 15:53:27 -0300

    feat: Add real routing - allow going to different pages/components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
 import Nav from './components/Nav';
 
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+
+//Router
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+
       <Nav />
-      <AboutUs />
+
+      <Routes>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="about-us" element={<AboutUs />} />
+        <Route path="our-work" element={<OurWork />} />
+        <Route path="contact-us" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <StyledNav>
       <h1>
-        <a id="logo" href="#">
+        <Link id="logo" to="/">
           Capture
-        </a>
+        </Link>
       </h1>
 
       <ul>
         <li>
-          <a href="#">1. About Us</a>
+          <Link className="link" to="/about-us">
+            1. About Us
+          </Link>
         </li>
         <li>
-          <a href="#">2. Our Work</a>
+          <Link className="link" to="/our-work">
+            2. Our Work
+          </Link>
         </li>
         <li>
-          <a href="#">3. Contact Us</a>
+          <Link className="link" to="/contact-us">
+            3. Contact Us
+          </Link>
         </li>
       </ul>
     </StyledNav>
