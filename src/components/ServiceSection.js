08d8ee2ce7f094facfa98e75c2b2c114bb8b2411
virgilionkanu <virgiliocano53@gmail.com>
2022-12-14 11:09:27 -0300

    change: Add style in service section component

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -6,54 +6,89 @@ import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
 import home2 from '../img/home2.png';
 
+import styled from 'styled-components';
+import { About, Description, Image, Hide } from '../styles';
+
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
           High <span>quality</span> services.
         </h2>
 
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={clock} alt="" />
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={teamwork} alt="" />
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={diaphragm} alt="" />
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={money} alt="" />
               <h3>Affordable</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </div>
+          </Card>
+        </Cards>
+      </Description>
 
-      <div className="image">
+      <Image>
         <img src={home2} alt="a camera" />
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 };
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+
+  p {
+    width: 70%;
+    padding: 2rem 0 4rem 0;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 20rem;
+
+  .icon {
+    display: flex;
+    align-items: center;
+
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServicesSection;
