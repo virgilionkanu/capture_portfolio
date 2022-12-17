8850d2cec59de2ef40a68ca3ecee64b01d1f1ae3
virgilionkanu <virgiliocano53@gmail.com>
2022-12-16 21:16:23 -0300

    feat: Add animation in about us page/component

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import home1 from '../img/home1.png';
 import { About, Description, Image, Hide } from '../styles';
 
 import { motion } from 'framer-motion';
+import { titleAnimation, fade, photoAnimation } from '../animation';
 
 const AboutSection = () => {
   return (
@@ -10,30 +11,34 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
 
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
 
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </Hide>
         </motion.div>
 
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professional with amazing skills to help you achieve it.
-        </p>
+        </motion.p>
 
-        <button>Contact Us</button>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
 
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img
+          variants={photoAnimation}
+          src={home1}
+          alt="guy with a camera"
+        />
       </Image>
     </About>
   );
