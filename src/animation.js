51f6e506e8352a7faa9c2937831f4ee2bf002614
virgilionkanu <virgiliocano53@gmail.com>
2022-12-17 12:26:11 -0300

    feat: Add animation into our-work page/component

--- src/animation.js
+++ src/animation.js
@@ -7,14 +7,13 @@ export const pageAnimation = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 1,
+      duration: 0.5,
       when: 'beforeChildren',
       staggerChildren: 0.25,
     },
   },
   exit: {
     opacity: 0,
-    y: 300,
     transition: {
       duration: 0.5,
     },
@@ -22,9 +21,7 @@ export const pageAnimation = {
 };
 
 export const titleAnimation = {
-  hidden: {
-    y: 200,
-  },
+  hidden: { y: 200 },
   show: {
     y: 0,
     transition: { duration: 0.75, ease: 'easeOut' },
@@ -35,7 +32,7 @@ export const fade = {
   hidden: { opacity: 0 },
   show: {
     opacity: 1,
-    transition: { duration: 0.75, ease: 'easeOut' },
+    transition: { ease: 'easeOut', duration: 0.75 },
   },
 };
 
@@ -44,6 +41,39 @@ export const photoAnimation = {
   show: {
     scale: 1,
     opacity: 1,
-    transition: { duration: 0.75, ease: 'easeOut' },
+    transition: { ease: 'easeOut', duration: 0.75 },
+  },
+};
+
+export const lineAnimation = {
+  hidden: { width: '0%' },
+  show: {
+    width: '100%',
+    transition: { duration: 1 },
+  },
+};
+
+export const slider = {
+  hidden: { x: '-130%', skew: '45deg' },
+  show: {
+    x: '100%',
+    skew: '0deg',
+    transition: { ease: 'easeOut', duration: 1 },
+  },
+};
+
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: { opacity: 1, transition: { staggerChildren: 0.15, ease: 'easeOut' } },
+};
+
+export const scrollReveal = {
+  hidden: { opacity: 0, scale: 1.2, transition: { duration: 0.5 } },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
   },
 };
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -7,7 +7,13 @@ import theracer from '../img/theracer-small.png';
 import goodtimes from '../img/goodtimes-small.png';
 
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import {
+  pageAnimation,
+  fade,
+  photoAnimation,
+  lineAnimation,
+  slider,
+} from '../animation';
 
 const OurWork = () => {
   return (
@@ -18,11 +24,17 @@ const OurWork = () => {
       animate="show"
       exit="exit"
     >
+      <Frame1 variants={slider}></Frame1>
+      <Frame2 variants={slider}></Frame2>
+      <Frame3 variants={slider}></Frame3>
+      <Frame4 variants={slider}></Frame4>
       <Movie>
-        <h2>The Athlete</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>The Athlete</motion.h2>
+        <motion.div variants={lineAnimation} className="line"></motion.div>
         <Link to="/our-work/the-athlete">
-          <img src={athlete} alt="athlete" />
+          <Hide>
+            <motion.img variants={photoAnimation} src={athlete} alt="athlete" />
+          </Hide>
         </Link>
       </Movie>
 
@@ -60,15 +72,42 @@ const Movie = styled.div`
 
   .line {
     height: 0.5rem;
-    background: #cccccc;
+    background: #23d997;
     margin-bottom: 3rem;
   }
 
   img {
-    width: 70%;
+    width: 100%;
     height: 70vh;
     object-fit: cover;
   }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+//Frame Animation
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
 `;
 
 export default OurWork;
